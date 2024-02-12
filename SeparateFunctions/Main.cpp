b69c2418eb23c1cdfc1cfd26519554aefafe0010
islahuddinm <pesonaalamdunia@gmail.com>
2024-02-12 21:11:35 +0700

    Initial Commit

---
+++ SeparateFunctions/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "01FunctionDeclars.h"
+
+int main() {
+
+    int result = other(4,5);
+
+    std::cout << "Result is " << result << '\n';
+    
+    other2();
+    return 0;
+}
